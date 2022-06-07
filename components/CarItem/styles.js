db47a88afab51ad9f178096ddbb89d6a7bf0e9aa
EddyM <Eddy.Mendoza01@student.csulb.edu>
2022-06-06 18:23:31 -0700

    update2

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const stlyes= StyleSheet.create ({
 
     carContainer:{
         width: '100%',
-        height: '100%',
+        height: Dimensions.get('window').height,
       },
       
     
